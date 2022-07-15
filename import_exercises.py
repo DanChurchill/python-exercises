b26414cf253118f38f6a6ba3bf8bdaea11487e94
Dan Churchill <dan.churchill.satx@gmail.com>
2022-07-15 11:00:36 -0500

    adding import exercises and associated data

---
+++ import_exercises.py
@@ -0,0 +1,6 @@
+# Create a file named import_exericses.py. 
+# Within this file, use from to import the calculate_tip function directly. 
+from function_exercises import calculate_tip
+
+# Call this function with values you choose and print the result.
+calculate_tip(.5, 100)
